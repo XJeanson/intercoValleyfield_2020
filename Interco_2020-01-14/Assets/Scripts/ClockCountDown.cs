b85e0cb176e5e80e4d284658c1a11faf40cae616
Xavier Jeanson <xavier_jeanson@hotmail.ca>
2020-01-14 22:01:16 -0500

    clockwork

---
+++ Interco_2020-01-14/Assets/Scripts/ClockCountDown.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockCountDown : MonoBehaviour
+{
+    public float startTime = 0f;
+    public float levelTime = 0f;
+    private float timeMultiplier = 0f;
+    private float lastUpdateTime = 0f;
+    private int multiplier = 100;
+    private float timer = 0;
+    private int H, M;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeMultiplier = ((19f - startTime) * 60) / levelTime;
+        H = (int)startTime;
+        M = (int)((startTime - (int)startTime) * multiplier);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > lastUpdateTime + 1)
+        {
+            M += (int)timeMultiplier;
+            if ((M % 60) == 0 || M >= 60)
+            {
+                H++;
+                M = 0;
+            }
+            GameObject.FindGameObjectWithTag("Clock").GetComponent<Text>().text = H + " : " + M;
+            lastUpdateTime = timer;
+        }
+    }
+}
